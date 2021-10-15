c862285802e4807d417cdae326d85b2c437e8318
Zhentian Wan <zhentian.wan@varian.com>
2021-10-15 13:39:01 +0300

    1-4: variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     extend: {},
   },
   variants: {
+    backgroundColor: ["responsive", "hover", "focus", "active"],
+    // for example you want to enable hover for the fontsize, normally you won't do that just an example
+    // hover:text-2xl
+    // fontSize: ['responsive', 'hover'],
     extend: {},
   },
   plugins: [],
-}
+};
